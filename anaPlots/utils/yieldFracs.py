63248e5e9e4664be41ce6e5eb0704785db621c83
lucasc896 <chris.lucas@bristol.ac.uk>
2013-02-11 14:32:43 +0100

    modified to take 5 and 10 percent variations

--- anaPlots/utils/yieldFracs.py
+++ anaPlots/utils/yieldFracs.py
@@ -12,38 +12,65 @@ from sys import argv
 #baseYield = "T2cc 220 145 & 158.0 $^{\pm 12.6 }$ & 69.0 $^{\pm 8.3 }$ & 44.0 $^{\pm 6.6 }$ & 18.0 $^{\pm 4.2 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
 #upYield = "T2cc 220 145 up5 & 162.0 $^{\pm 12.7 }$ & 60.0 $^{\pm 7.7 }$ & 49.0 $^{\pm 7.0 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
 #downYield = "T2cc 220 145 down5 & 169.0 $^{\pm 13.0 }$ & 75.0 $^{\pm 8.7 }$ & 44.0 $^{\pm 6.6 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
-
-baseYield = argv[1]
-upYield = argv[2]
-if len(argv)>3: downYield = argv[3]
-else: downYield=downYield = "T2cc 220 145 down5 & 169.0 $^{\pm 13.0 }$ & 75.0 $^{\pm 8.7 }$ & 44.0 $^{\pm 6.6 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
+print len(argv)
+if len(argv)>4:
+   upSYield = argv[1]
+   upYield = argv[2]
+   baseYield = argv[3]
+   downYield = argv[4]
+   downSYield = argv[5]
+else:
+   upSYield = "T2cc 220 145 down5 & 169.0 $^{\pm 13.0 }$ & 75.0 $^{\pm 8.7 }$ & 44.0 $^{\pm 6.6 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
+   upYield = argv[1]
+   baseYield = argv[2]
+   downYield = argv[3]
+   downSYield = "T2cc 220 145 down5 & 169.0 $^{\pm 13.0 }$ & 75.0 $^{\pm 8.7 }$ & 44.0 $^{\pm 6.6 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
+#if len(argv)>3: downYield = argv[3]
+#else: downYield = "T2cc 220 145 down5 & 169.0 $^{\pm 13.0 }$ & 75.0 $^{\pm 8.7 }$ & 44.0 $^{\pm 6.6 }$ & 17.0 $^{\pm 4.1 }$ & 0.0 $^{\pm 0.0 }$ & 2.0 $^{\pm 1.4 }$ & 0.0 $^{\pm 0.0 }$ & 0.0 $^{\pm 0.0 }$  \\"
 
 splBase = baseYield.split(" & ")
 splUp = upYield.split(" & ")
+splSUp = upSYield.split(" & ")
 splDown = downYield.split(" & ")
-
+splSDown = downSYield.split(" & ")
 
 upFrac = []
+upSFrac = []
 downFrac = []
+downSFrac = []
 
-for stringBase, stringUp, stringDown in zip(splBase[1:], splUp[1:], splDown[1:]):
+for stringBase, stringUp, stringSUp, stringDown, stringSDown in zip(splBase[1:], splUp[1:], splSUp[1:], splDown[1:], splSDown[1:]):
    valBase = stringBase.split(" ")[0]
    valUp = stringUp.split(" ")[0]
+   valSUp = stringSUp.split(" ")[0]
    valDown = stringDown.split(" ")[0]
-
+   valSDown = stringSDown.split(" ")[0]
+   
    if float(valBase)==0:
       upFrac.append(0)
+      upSFrac.append(0)
       downFrac.append(0)
+      downSFrac.append(0)
    else:
       upFrac.append( float(valUp)/float(valBase) )
+      upSFrac.append( float(valSUp)/float(valBase) )
       downFrac.append( float(valDown)/float(valBase) )
+      downSFrac.append( float(valSDown)/float(valBase) )
 
-upLine = splBase[0]+" UpFrac & "
-downLine = splBase[0]+" DownFrac & "
+upLine = splBase[0]+" Up5 Frac & "
+upSLine = splBase[0]+" Up10 Frac & "
+downLine = splBase[0]+" Down5 Frac & "
+downSLine = splBase[0]+" Down10 Frac & "
 
-for up, down in zip(upFrac, downFrac):
+for up, sup, down, sdown in zip(upFrac, upSFrac, downFrac, downSFrac):
    upLine += "%.5s & "%str(up)
+   upSLine += "%.5s & "%str(sup)
    downLine += "%.5s & "%str(down)
+   downSLine += "%.5s & "%str(sdown)
 
-if len(argv)>3: print "\n"+downLine[:-2]+" \\\\"
+#if len(argv)>3: print "\n"+downLine[:-2]+" \\\\"
+
+if len(argv)>4: print upSLine[:-2]+" \\\\"+"\n"
 print upLine[:-2]+" \\\\"+"\n"
+if len(argv)>4: print downSLine[:-2]+" \\\\"+"\n"
+print downLine[:-2]+" \\\\"+"\n"
