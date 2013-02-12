c23d5a1835a40ee6d688574c046afe090b486579
lucasc896 <chris.lucas@bristol.ac.uk>
2013-02-12 08:24:50 +0100

    add new ISR system op

--- ICFCode/src/common/Python.cc
+++ ICFCode/src/common/Python.cc
@@ -1,15 +1,19 @@
 #include <boost/python.hpp>
 #include "charmEffStudy.hh"
 #include "analysisPlots.hh"
+#include "ISRSystematic.hh"
 
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(lib_charmStudy) {
 
- class_<Operation::charmEffStudy, bases<Operation::_Base> >( "OP_charmEffStudy",
-                    init<const Utils::ParameterSet&>());
+   class_<Operation::charmEffStudy, bases<Operation::_Base> >( "OP_charmEffStudy",
+                     init<const Utils::ParameterSet&>());
 
- class_<Operation::analysisPlots, bases<Operation::_Base> >( "OP_analysisPlots",
-                    init<const Utils::ParameterSet&>());
+   class_<Operation::analysisPlots, bases<Operation::_Base> >( "OP_analysisPlots",
+                     init<const Utils::ParameterSet&>());
+
+   class_<Operation::ISRSystematic, bases<Operation::_Base> >( "OP_ISRSystematic",
+                     init<const Utils::ParameterSet&>());
 
 }
