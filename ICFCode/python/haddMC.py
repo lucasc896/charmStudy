8ee9f2de96789e581e0ea1d0aea15b35adb5b84e
lucasc896 <chris.lucas@bristol.ac.uk>
2013-02-12 08:23:12 +0100

    add more intelligent splitting

--- ICFCode/python/haddMC.py
+++ ICFCode/python/haddMC.py
@@ -6,24 +6,32 @@ import subprocess
 
 def doHadd(fList=None, label="", ana="anaPlots"):
 
-    print " >>> Doing hadd for %s"%(label)
+    print "\n >>> Doing hadd for %s"%(label)
 
-    myStr=""
+    cmd = []
+
+    c = commands.getstatusoutput("ls ../haddOut/out%s_%s.root"%(label, ana))
+    if "No such file" not in c[1]:
+        print "\n\t***Removing old file %s\n"%label
+        cmd.append("rm")
+        cmd.append("../haddOut/out%s_%s.root"%(label, ana))
+        subprocess.call(cmd)
+        cmd = []
+
     for i in fList:
-        myStr += " %s"%i 
-        #myStr.append(i)
+        cmd.append(i)
+    cmd.insert(0, "../haddOut/out%s_%s.root"%(label, ana))
+    cmd.insert(0, "hadd")
+    subprocess.call(cmd)
 
-    #print myStr
-    cmd = ["hadd", "../haddOut/out%s_%s.root"%(label, ana), myStr]
-    #subprocess.call(cmd)
+parDir = "../results_27_Nov"
+dirs = ["275_", "325_", "375_"]
+lsOut = []
 
-    print cmd
-
-
-
-tmp = commands.getstatusoutput("ls ../results_14_Nov_06/375_/*root")
-tmp = tmp[1:]
-tmp = tmp[0].split("\n")
+for d in dirs:
+    tmp = commands.getstatusoutput("ls %s/%s/*root"%(parDir, d))
+    tmp = tmp[1].split("\n")
+    if len(tmp)>1: lsOut += tmp
 
 fWJets = []
 fQCD   = []
@@ -32,18 +40,18 @@ fSinT  = []
 fDiBo  = []
 fTTbar = []
 
-for f in tmp:
+for f in lsOut:
     if "WJet" in f:
         fWJets.append(f)
     if "QCD" in f:
         fQCD.append(f)
     if "ZJets" in f:
         fZinv.append(f)
-    if "_T_" or "_Tbar_" in f:
+    if "_T_" in f or "_Tbar_" in f:
         fSinT.append(f)
-    if "_ZZ_" or "_WW_" or "_WZ_" in f:
+    if "_ZZ_" in f or "_WW_" in f or "_WZ_" in f:
         fDiBo.append(f)
-    if "_TTJets" in f:
+    if "_TT_CT10_" in f:
         fTTbar.append(f)
 
 if fWJets: doHadd(fWJets, "WJets")
