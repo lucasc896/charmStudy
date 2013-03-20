d4c1fcd338738196b50433cebbb489a0bfddf4fb
lucasc896 <chris.lucas@bristol.ac.uk>
2013-03-20 10:01:32 +0100

    add new charm filters and entry for StopGenVect cut

--- ICFCode/src/common/Python.cc
+++ ICFCode/src/common/Python.cc
@@ -2,18 +2,22 @@
 #include "charmEffStudy.hh"
 #include "analysisPlots.hh"
 #include "ISRSystematic.hh"
+#include "charmFilters.hh"
 
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(lib_charmStudy) {
 
    class_<Operation::charmEffStudy, bases<Operation::_Base> >( "OP_charmEffStudy",
-                     init<const Utils::ParameterSet&>());
+                     init<const Utils::ParameterSet&>() );
 
    class_<Operation::analysisPlots, bases<Operation::_Base> >( "OP_analysisPlots",
-                     init<const Utils::ParameterSet&>());
+                     init<const Utils::ParameterSet&>() );
 
    class_<Operation::ISRSystematic, bases<Operation::_Base> >( "OP_ISRSystematic",
-                     init<const Utils::ParameterSet&>());
+                     init<const Utils::ParameterSet&>() );
+
+   class_<Operation::StopGenVectCut, bases<Operation::_Base> >( "OP_StopGenVectPtSumCut",
+                     init<float>() );
 
 }
