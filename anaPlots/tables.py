6eb93fe340971cffbc6d6eabc7e410a1e02e5291
lucasc896 <chris.lucas@bristol.ac.uk>
2013-03-26 14:23:14 +0100

    pep-8 changes

--- anaPlots/tables.py
+++ anaPlots/tables.py
@@ -7,7 +7,6 @@ Created by Chris Lucas on 2012-11-14.
 Copyright (c) 2012 University of Bristol. All rights reserved.
 """
 
-from sys import argv, exit
 from generalUtils import *
 from Log import *
 import configuration as conf
@@ -18,123 +17,124 @@ import re
 
 ###-------------------------------------------------------------------###
 
+
 def getbMultis(bM=""):
   bMultiHists = {
-    "0b":["_0"],
-    "1b":["_1"],
-    "2b":["_2"],
-    "3b":["_3"],
-    "4b":["_4"],
-    "ge1b":["_1", "_2", "_3", "_4", "_5"],
-    "ge2b":["_2", "_3", "_4", "_5"],
-    "ge3b":["_3", "_4", "_5"],
-    "ge4b":["_4", "_5"],
-    "inc":["_0", "_1", "_2", "_3", "_4", "_5"],}
+      "0b": ["_0"],
+      "1b": ["_1"],
+      "2b": ["_2"],
+      "3b": ["_3"],
+      "4b": ["_4"],
+      "ge1b": ["_1", "_2", "_3", "_4", "_5"],
+      "ge2b": ["_2", "_3", "_4", "_5"],
+      "ge3b": ["_3", "_4", "_5"],
+      "ge4b": ["_4", "_5"],
+      "inc": ["_0", "_1", "_2", "_3", "_4", "_5"]}
 
   return bMultiHists[bM]
 
 ###-------------------------------------------------------------------###
 
+
 def getSMPred(bM="inc", jM="inc"):
 
-  zerobtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[6235., 100., 1010., 34.],
-          "325-375":[2900., 60., 447., 19.],
-          "375-475":[1955., 34., 390., 19.],
-          "475-575":[558., 14., 250., 12.],
-          "575-675":[186., 11., 111., 9.],
-          "675-775":[51.3, 3.4, 53.3, 4.3],
-          "775-875":[21.2, 2.3, 18.5, 2.4],
-          "875-inf":[16.1, 1.7, 19.4, 2.5],
+  zerobtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [6235., 100., 1010., 34.],
+      "325-375": [2900., 60., 447., 19.],
+      "375-475": [1955., 34., 390., 19.],
+      "475-575": [558., 14., 250., 12.],
+      "575-675": [186., 11., 111., 9.],
+      "675-775": [51.3, 3.4, 53.3, 4.3],
+      "775-875": [21.2, 2.3, 18.5, 2.4],
+      "875-inf": [16.1, 1.7, 19.4, 2.5],
   }
 
-  onebtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[1162, 37., 521., 25.],
-          "325-375":[481., 18., 232., 15.],
-          "375-475":[341., 15., 188., 12.],
-          "475-575":[86.7, 4.2, 106., 6.],
-          "575-675":[24.8, 2.8, 42.1, 4.1],
-          "675-775":[7.2, 1.1, 17.9, 2.2],
-          "775-875":[3.3, 0.7, 9.8, 1.5],
-          "875-inf":[2.1, 0.5, 6.8, 1.2],
+  onebtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [1162, 37., 521., 25.],
+      "325-375": [481., 18., 232., 15.],
+      "375-475": [341., 15., 188., 12.],
+      "475-575": [86.7, 4.2, 106., 6.],
+      "575-675": [24.8, 2.8, 42.1, 4.1],
+      "675-775": [7.2, 1.1, 17.9, 2.2],
+      "775-875": [3.3, 0.7, 9.8, 1.5],
+      "875-inf": [2.1, 0.5, 6.8, 1.2],
   }
-  twobtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[224., 15., 208., 17.],
-          "325-375":[98.2, 8.4, 103., 9.],
-          "375-475":[59., 5.2, 85.9, 7.2],
-          "475-575":[12.8, 1.6, 51.7, 4.6],
-          "575-675":[3., 0.9, 19.9, 3.4],
-          "675-775":[.5, 0.2, 6.8, 1.2],
-          "775-875":[0.1, 0.1, 1.7, 0.7],
-          "875-inf":[0.1, 0., 1.3, 0.4],
+  twobtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [224., 15., 208., 17.],
+      "325-375": [98.2, 8.4, 103., 9.],
+      "375-475": [59., 5.2, 85.9, 7.2],
+      "475-575": [12.8, 1.6, 51.7, 4.6],
+      "575-675": [3., 0.9, 19.9, 3.4],
+      "675-775": [.5, 0.2, 6.8, 1.2],
+      "775-875": [0.1, 0.1, 1.7, 0.7],
+      "875-inf": [0.1, 0., 1.3, 0.4],
   }
-  threebtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[0., 0., 25.3, 5.],
-          "325-375":[0., 0., 11.7, 1.7],
-          "375-475":[0., 0., 6.7, 1.4],
-          "475-575":[0., 0., 3.9, 0.8],
-          "575-675":[0., 0., 2.3, 0.6],
-          "675-775":[0., 0., 1.2, 0.3],
-          "775-875":[0., 0., 0.3, 0.2],
-          "875-inf":[0., 0., 0.1, 0.1],
+  threebtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [0., 0., 25.3, 5.],
+      "325-375": [0., 0., 11.7, 1.7],
+      "375-475": [0., 0., 6.7, 1.4],
+      "475-575": [0., 0., 3.9, 0.8],
+      "575-675": [0., 0., 2.3, 0.6],
+      "675-775": [0., 0., 1.2, 0.3],
+      "775-875": [0., 0., 0.3, 0.2],
+      "875-inf": [0., 0., 0.1, 0.1],
   }
-  fourbtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[0., 0., 0.9, 0.4],
-          "325-375":[0., 0., 0.3, 0.2],
-          "375-475":[0., 0., 0.6, 0.3],
-          "475-575":[0., 0., 0., 0.],
-          "575-675":[0., 0., 0., 0.],
-          "675-775":[0., 0., 0., 0.],
-          "775-875":[0., 0., 0., 0.],
-          "875-inf":[0., 0., 0., 0.],
+  fourbtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [0., 0., 0.9, 0.4],
+      "325-375": [0., 0., 0.3, 0.2],
+      "375-475": [0., 0., 0.6, 0.3],
+      "475-575": [0., 0., 0., 0.],
+      "575-675": [0., 0., 0., 0.],
+      "675-775": [0., 0., 0., 0.],
+      "775-875": [0., 0., 0., 0.],
+      "875-inf": [0., 0., 0., 0.],
   }
-  inclbtagDict={
-          "225-275":[0., 0., 0., 0.],
-          "275-325":[0., 0., 0., 0.],
-          "325-375":[0., 0., 0., 0.],
-          "375-475":[0., 0., 0., 0.],
-          "475-575":[0., 0., 0., 0.],
-          "575-675":[0., 0., 0., 0.],
-          "675-775":[0., 0., 0., 0.],
-          "775-875":[0., 0., 0., 0.],
-          "875-inf":[0., 0., 0., 0.], 
+  inclbtagDict = {
+      "225-275": [0., 0., 0., 0.],
+      "275-325": [0., 0., 0., 0.],
+      "325-375": [0., 0., 0., 0.],
+      "375-475": [0., 0., 0., 0.],
+      "475-575": [0., 0., 0., 0.],
+      "575-675": [0., 0., 0., 0.],
+      "675-775": [0., 0., 0., 0.],
+      "775-875": [0., 0., 0., 0.],
+      "875-inf": [0., 0., 0., 0.],
   }
 
   HTbins = [
-          "225-275",
-          "275-325",
-          "325-375",
-          "375-475",
-          "475-575",
-          "575-675",
-          "675-775",
-          "775-875",
-          "875-inf",
-        ]
-
+      "225-275",
+      "275-325",
+      "325-375",
+      "375-475",
+      "475-575",
+      "575-675",
+      "675-775",
+      "775-875",
+      "875-inf",
+  ]
 
   #sum all dictionaries to make inclusive dict
   for i in HTbins:
     for k in range(4):
-      if k==0 or k==2:
-        inclbtagDict[i][k]+=zerobtagDict[i][k]
-        inclbtagDict[i][k]+=onebtagDict[i][k]
-        inclbtagDict[i][k]+=twobtagDict[i][k]
-        inclbtagDict[i][k]+=threebtagDict[i][k]
-        inclbtagDict[i][k]+=fourbtagDict[i][k]
-      if k==1 or k==3:
-        inclbtagDict[i][k]+=zerobtagDict[i][k]*zerobtagDict[i][k]
-        inclbtagDict[i][k]+=onebtagDict[i][k]*onebtagDict[i][k]
-        inclbtagDict[i][k]+=twobtagDict[i][k]*twobtagDict[i][k]
-        inclbtagDict[i][k]+=threebtagDict[i][k]*threebtagDict[i][k]
-        inclbtagDict[i][k]+=fourbtagDict[i][k]*fourbtagDict[i][k]
-    inclbtagDict[i][1]=math.sqrt(inclbtagDict[i][1])
-    inclbtagDict[i][3]=math.sqrt(inclbtagDict[i][3])
+      if k == 0 or k == 2:
+        inclbtagDict[i][k] += zerobtagDict[i][k]
+        inclbtagDict[i][k] += onebtagDict[i][k]
+        inclbtagDict[i][k] += twobtagDict[i][k]
+        inclbtagDict[i][k] += threebtagDict[i][k]
+        inclbtagDict[i][k] += fourbtagDict[i][k]
+      if k == 1 or k == 3:
+        inclbtagDict[i][k] += zerobtagDict[i][k]*zerobtagDict[i][k]
+        inclbtagDict[i][k] += onebtagDict[i][k]*onebtagDict[i][k]
+        inclbtagDict[i][k] += twobtagDict[i][k]*twobtagDict[i][k]
+        inclbtagDict[i][k] += threebtagDict[i][k]*threebtagDict[i][k]
+        inclbtagDict[i][k] += fourbtagDict[i][k]*fourbtagDict[i][k]
+    inclbtagDict[i][1] = math.sqrt(inclbtagDict[i][1])
+    inclbtagDict[i][3] = math.sqrt(inclbtagDict[i][3])
 
   zerobtagDict  = OrderedDict(sorted(zerobtagDict.items(), key=lambda t: t[0]))
   onebtagDict   = OrderedDict(sorted(onebtagDict.items(), key=lambda t: t[0]))
@@ -143,23 +143,26 @@ def getSMPred(bM="inc", jM="inc"):
   fourbtagDict  = OrderedDict(sorted(fourbtagDict.items(), key=lambda t: t[0]))
   inclbtagDict  = OrderedDict(sorted(inclbtagDict.items(), key=lambda t: t[0]))
 
-  if bM=="0b":
+  if bM == "0b":
     smDict = zerobtagDict
-  if bM=="1b":
+  if bM == "1b":
     smDict = onebtagDict
-  if bM=="inc":
+  if bM == "inc":
     smDict = inclbtagDict
-  
+
   for key, val in smDict.iteritems():
-    if jM=="le3j": val = val[:2]
-    if jM=="le3j": val = val[2:]
-    if jM=="inc":
-      val[0]=val[0]+val[2]
-      val[1]=math.sqrt(val[1]*val[1] + val[3]*val[3])
+    if jM == "le3j":
+      val = val[:2]
+    if jM == "le3j":
+      val = val[2:]
+    if jM == "inc":
+      val[0] = val[0]+val[2]
+      val[1] = math.sqrt(val[1]*val[1] + val[3]*val[3])
 
   return smDict
 
 ###-------------------------------------------------------------------###
+
 
 def getDataYields(bM="inc", debug=False):
 
@@ -180,56 +183,54 @@ def getDataYields(bM="inc", debug=False):
   regex = re.compile(r"._mSt(\d*)_mL(\d*)")
   result = regex.findall(sigSamp)
 
-  masses = result[0] if len(result)>0 else []
-
+  masses = result[0] if len(result) > 0 else []
 
   # enter loop if both mStop and mLSP are matched
   # note: eventually make possible for single mass scenario (i.e. an sms strip)
-  if len(masses)>1:
-  
+  if len(masses) > 1:
+
     # get stop mass
     # note: eventually change to lookup table
     if masses[0] == "200":
       xSec = 18.5245
-      print "xSec 18.5245"
-      
+
       # get nEvents
       # note: eventually change to lookup search
       if masses[1] == "120":
         nSource = 630563.
       elif masses[1] == "190":
         nSource = 630587.
-      print nSource    
 
     else:
       xSec = 1.
 
     scale = targetLumi*xSec/nSource
-  
+
   else:
     scale = 1
 
-  Log.warning("Tables filled with scale factor %f"%scale)
+  Log.warning("Tables filled with scale factor %f" % scale)
 
   for d in dirs:
-    ent=0
-    dirTitle = d[4:].replace("_","-")
-    if d=="inc_875": dirTitle="875-inf"
+    ent = 0
+    dirTitle = d[4:].replace("_", "-")
+    if d == "inc_875": dirTitle = "875-inf"
 
     for suf in getbMultis(bM):
-      if debug: Log.debug("Getting %s/commHT%s"%(d, suf))
-      h = sFile.Get("%s/commHT%s"%(d, suf))
+      if debug: Log.debug("Getting %s/commHT%s" % (d, suf))
+      h = sFile.Get("%s/commHT%s" % (d, suf))
       ent += h.GetEntries()
       if debug: Log.debug(str(ent))
-    ent *= scale  
-    yieldDict[dirTitle]=[ent, math.sqrt(ent)]
+    ent *= scale
+    yieldDict[dirTitle] = [ent, math.sqrt(ent)]
 
   return OrderedDict(sorted(yieldDict.items(), key=lambda t: t[0])), scale
 
 ###-------------------------------------------------------------------###
 
+
 def printHeader():
-  outTxt=""
+  outTxt = ""
   outTxt += "\\documentclass[a4paper,12pt]{article}\n"
   outTxt += "\\usepackage[margin=0.3in, landscape]{geometry}\n"
   outTxt += "\\begin{document}\n\n"
@@ -239,24 +240,27 @@ def printHeader():
 
 ###-------------------------------------------------------------------###
 
+
 def printCaption(bM, jM):
 
-  if jM=="le3j":
+  if jM == "le3j":
     label = "$N_{jet} \\leq 3$"
-  elif jM=="ge4j":
+  elif jM == "ge4j":
     label = "$N_{jet} \\leq 3$"
   else:
     label = "all jets"
 
-  outTxt = "\\caption{Yields for $\\alpha_T>$ 0.55 (%s b-jets, %s)}\n"%(bM, label)
+  outTxt = "\\caption{Yields for $\\alpha_T>$ 0.55 (%s b-jets, %s)}\n" % (bM, label)
   #add a bit more eventually
 
   return outTxt
 
 ###-------------------------------------------------------------------###
 
+
 def printEnd():
-  outTxt  = "\n\n\n"
+
+  outTxt = "\n\n\n"
   outTxt += "\\end{tabular}\n"
   outTxt += "\\end{center}\n"
   outTxt += "\\end{table}\n"
@@ -265,6 +269,7 @@ def printEnd():
   return outTxt
 
 ###-------------------------------------------------------------------###
+
 
 def printHT():
 
@@ -277,25 +282,24 @@ def printHT():
     split.insert(1, " 225-275 ")
     HTline = " & ".join(split)
 
-  print HTline
-
   return HTline
 
 ###-------------------------------------------------------------------###
 
+
 def makeTable(bM="inc", debug=False):
-  
+
   sigSamp  = conf.switches()["signalSample"]
   jM       = conf.switches()["jetMulti"]
 
-  Log.info("\tBin: %s %s\n"%(bM, jM))
+  Log.info("\tBin: %s %s\n" % (bM, jM))
 
   smPred = getSMPred(bM, jM)
   dYield, scale = getDataYields(bM, debug=debug)
 
   if debug: Log.debug("Opening: tableDump/yieldTable_%s_%s_%s.tex"%(sigSamp, jM, bM))
 
-  f = open("tableDump/yieldTable_%s_%s_%s.tex"%(sigSamp, jM, bM), "w")
+  f = open("tableDump/yieldTable_%s_%s_%s.tex" % (sigSamp, jM, bM), "w")
   outTxt = ""
   outTxt += printHeader()
   outTxt += printCaption(bM, jM)
@@ -305,7 +309,7 @@ def makeTable(bM="inc", debug=False):
 
   outTxt += printHT()
 
-  outTxt +="\\hline"
+  outTxt += "\\hline"
 
   print "\n*** SM BG Pred ***"
 
@@ -314,26 +318,25 @@ def makeTable(bM="inc", debug=False):
   for key, val in smPred.iteritems():
     if "parked" not in conf.switches()["HTcuts"] and "225" in key:
       continue
-    print "%s\t%f \\pm %f"%(key, val[0], val[1])
-    outTxt += "& %.1f $^{\pm %.1f }$ "%(val[0], val[1])
+    print "%s\t%f \\pm %f" % (key, val[0], val[1])
+    outTxt += "& %.1f $^{\pm %.1f }$ " % (val[0], val[1])
 
   outTxt += " \\\\"
 
-  print "\n*** %s Yields"%(sigSamp)
-  outTxt += "\n\n%s "%sigSamp.replace("_"," ")
+  print "\n*** %s Yields" % (sigSamp)
+  outTxt += "\n\n%s " % sigSamp.replace("_", " ")
 
   totYield = 0
   for key, val in dYield.iteritems():
-    print "%s\t%f \\pm %f"%(key, val[0], val[1])
-    outTxt += "& %.1f $^{\pm %.1f }$ "%(val[0], val[1])
+    print "%s\t%f \\pm %f" % (key[1:], val[0], val[1])
+    outTxt += "& %.1f $^{\pm %.1f }$ " % (val[0], val[1])
     totYield += val[0]
 
-  print "\n*** Total Yield for %s: %d ***\n"%(sigSamp, totYield)
+  print "\n*** Total Yield for %s: %d ***\n" % (sigSamp, totYield)
   outTxt += " \\\\"
 
-  outTxt += "\n%%Total: %f"%totYield
+  outTxt += "\n%%Total: %f" % totYield
   outTxt += "\n%%Filled with Scale: %f" % scale
-
 
   outTxt += printEnd()
 
@@ -341,16 +344,16 @@ def makeTable(bM="inc", debug=False):
 
 ###-------------------------------------------------------------------###
 
+
 def printTable(debug=False):
-  
+
   bMulti = conf.bMulti()
 
-  if len( bMulti )>1:
+  if len(bMulti) > 1:
      print "\n"
      Log.error("*** ERROR: Too many b-tag multiplicities for yield mode. ***")
      Log.error("\tChange 'bMulti()' in configurtion.py\n")
      sys.exit()
-      
 
   for b in bMulti:
     makeTable(b, debug=debug)
