eb3b4a7744ba521c9f8b4be9877d3c29d23dfed3
lucasc896 <chris.lucas@bristol.ac.uk>
2013-04-22 22:03:11 +0200

    add file containing stop and gluino production xsec

---
+++ anaPlots/xSec.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+xSec.py
+
+Created by Chris Lucas on 2013-04-22.
+Copyright (c) 2012 University of Bristol. All rights reserved.
+"""
+
+# values taken from:
+# https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections8TeVstopsbottom
+
+stopXSec = {
+    125: 197.122,
+    150: 80.268,
+    175: 36.7994,
+    200: 18.5245,
+    225: 9.90959,
+    250: 5.57596,
+        }
+
+# value taken from:
+# https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections8TeVgluglu
+
+gluonXSec = {
+    200: 1010.1,
+    225: 540.437,
+    250: 302.3,
+    275: 176.213,
+    300: 106.269,
+        }
