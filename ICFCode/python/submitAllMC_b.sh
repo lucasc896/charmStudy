1175b28ce1d5cefde02511a502f3127f2f47456b
lucasc896 <chris.lucas@bristol.ac.uk>
2013-05-02 16:53:39 +0200

    remove old submission scripts; add new deadEcal dir

--- ICFCode/python/submitAllMC_b.sh
+++
@@ -1,7 +0,0 @@
-echo "\n*** Submitting all MC with batch ***\n\n"
-
-./mcSubmit.py -b -n50
-./mcSubmit43.py -b -n50
-./mcSubmit37.py -b -n50
-./mcSubmit30.py -b -n50
-./mcSubmit_noCuts.py -b -n50
--- ICFCode/python/submitAllMC_noB.sh
+++
@@ -1,7 +0,0 @@
-echo "\n*** Submitting all MC with NO batch ***\n\n"
-
-./mcSubmit.py
-./mcSubmit43.py
-./mcSubmit37.py
-./mcSubmit30.py
-./mcSubmit_noCuts.py
