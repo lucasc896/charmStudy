6637021476b639414ab5bf54076c0fb84181ddb9
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-07-20 12:15:32 +0200

    new isoTracks python OP

--- ICFCode/src/common/Python.cc
+++ ICFCode/src/common/Python.cc
@@ -1,6 +1,7 @@
 #include <boost/python.hpp>
 #include "charmEffStudy.hh"
 #include "analysisPlots.hh"
+#include "isoTrackPlots.hh"
 #include "charmFilters.hh"
 
 using namespace boost::python;
@@ -12,6 +13,9 @@ BOOST_PYTHON_MODULE(lib_charmStudy) {
 
    class_<Operation::analysisPlots, bases<Operation::_Base> >( "OP_analysisPlots",
                      init<const Utils::ParameterSet&>() );
+
+   class_<Operation::isoTrackPlots, bases<Operation::_Base> >( "OP_isoTrackPlots",
+		     init<const Utils::ParameterSet&>() );
 
    class_<Operation::StopGenVectCut, bases<Operation::_Base> >( "OP_StopGenVectPtSumCut",
                      init<float>() );
