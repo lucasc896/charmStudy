a41574953861855d1ef613ac771992d48dc1e093
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-07-20 12:14:23 +0200

    new plots; remove old plots; add in new bool flags

--- ICFCode/include/analysisPlots.hh
+++ ICFCode/include/analysisPlots.hh
@@ -36,7 +36,9 @@ namespace Operation {
       double minDR_;
       bool StandardPlots_;
       double threshold_;      
-      bool NoCuts_; 
+      bool NoCuts_;
+      bool isData_;
+      bool Debug_;
  
       void StandardPlots();
       bool StandardPlots( Event::Data& ev );
@@ -47,38 +49,32 @@ namespace Operation {
       vector<double> getStopGenPt( Event::Data& ev );
       double getGenDeltaPhi( const Event::GenObject& gOb1, const Event::GenObject& gOb2 );
       int verticesN( Event::Data& ev );
+      int getNIsoTrack( Event::Data& ev );
   
       // Histos
       std::vector<TH1D*>  h_nEvents;
       std::vector<TH1D*>  h_evWeight;
       std::vector<TH1D*>  h_nJets;
-      std::vector<TH1D*>  h_nJets_charm;
-      std::vector<TH1D*>  h_nJets_ISR;
       std::vector<TH1D*>  h_nBTagJets;
       std::vector<TH1D*>  h_jetPt;
       std::vector<TH1D*>  h_charmJetPt_0;
       std::vector<TH1D*>  h_charmJetPt_1;
       std::vector<TH1D*>  h_leadJetPt;
       std::vector<TH1D*>  h_subLeadJetPt;
-      std::vector<TH1D*>  h_leadISRJetPt;
-      std::vector<TH1D*>  h_subLeadISRJetPt;      
       std::vector<TH1D*>  h_thirdJetPt;
       std::vector<TH1D*>  h_fourthJetPt;
       std::vector<TH1D*>  h_fivePlusJetPt;
       std::vector<TH1D*>  h_commHT;
-      std::vector<TH1D*>  h_HT_charm;
-      std::vector<TH1D*>  h_HT_ISR;
       std::vector<TH1D*>  h_MET;
       std::vector<TH1D*>  h_MHT;
       std::vector<TH1D*>  h_MHToverMET;
       std::vector<TH1D*>  h_MHToverHT;
       std::vector<TH1D*>  h_hadronicAlphaT;
       std::vector<TH1D*>  h_hadronicAlphaTZoom;
+      std::vector<TH1D*>  h_genPartonHT;
       std::vector<TH1D*>  h_leadJetdelPhi;
       std::vector<TH1D*>  h_stopGenPtVect;
       std::vector<TH1D*>  h_stopGenPtScal;
-      //std::vector<TH2D*>  h_delPhi_vs_scalGenPt;
-      //std::vector<TH2D*>  h_delPhi_vs_vectGenPt;
       std::vector<TH1D*>  h_dPhiStopCharm;
       std::vector<TH1D*>  h_dPhiNeutCharm;
       std::vector<TH1D*>  h_dPhiStopStop;
@@ -87,21 +83,20 @@ namespace Operation {
       std::vector<TH1D*>  h_dPhiLeadJetMHT;
       std::vector<TH1D*>  h_dPhiSubLeadJetMHT;
       std::vector<TH2D*>  h_susyScanPlane;
-      //std::vector<TH2D*>  h_SMSvectGenPt;
-      //std::vector<TH2D*>  h_SMSscalGenPt;
-      //std::vector<TH2D*>  h_SMSdPhiLeadJetsGenPt;
-      //std::vector<TH2D*>  h_SMSAlphaT;
-      //std::vector<TH2D*>  h_alphaT_vs_HT;
-      //std::vector<TH2D*>  h_leadJetPt_vs_HT;
-      //std::vector<TH2D*>  h_leadminsubJetPt_vs_HT; 
+      std::vector<TH2D*>  h_alphaT_vs_HT;
       std::vector<TH2D*>  h_vectGenPt_vs_scalGenPt;
-      std::vector<TH2D*>  h_genPtLeadCharm_vs_MHT;
-      std::vector<TH2D*>  h_delPhiLeadJetMHT_vs_MHT;  
       std::vector<TH1D*>  h_leadTwoJetsPt;  
       std::vector<TH2D*>  h_commHT_vs_nVtx;
       std::vector<TH2D*>  h_MHT_vs_nVtx;
       std::vector<TH2D*>  h_jetPt_vs_nVtx; 
       std::vector<TH1D*>  h_nVertex;
+      std::vector<TH1D*>  h_nIsoTrack;
+
+      std::vector<TH1D*>  h_pfCandsPtDiff;
+      std::vector<TH1D*>  h_pfCandsPt;
+      std::vector<TH1D*>  h_pfCandsEta;
+      std::vector<TH1D*>  h_pfCandsDzPV;
+      std::vector<TH1D*>  h_pfCandsDunno;
   };
 
 }
