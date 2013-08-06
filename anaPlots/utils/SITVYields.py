16477f6e0c9c2ffd9bbdc3e506d7c79cc8703323
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-08-06 11:36:05 +0200

    more samples; change the definitions of efficiencies; start introducing new grapher class

--- anaPlots/utils/SITVYields.py
+++ anaPlots/utils/SITVYields.py
@@ -2,14 +2,17 @@
 # encoding: utf-8
 
 import ROOT as r
+import generalUtils as gutils
 from sys import argv
 from sys import exit
 from Log import *
 
+r.gROOT.SetBatch(r.kTRUE)
+
 def switches():
 
   switches={
-          "signalSample"  :"TTbar_isoTrack",
+          "sample"        :["WJets", "TTbar", "DY", "ZJets", "T2cc_200_120"][2],
           "HTcuts"        :["noCutInc", "standardHT","highHT","lowHT","parkedHT"][1],
           "jetMulti"      :["le3j","ge4j","inc","after","before"][-1],
           "norm"          :["None", "Unitary", "xSec", "lumi"][0],
@@ -25,7 +28,7 @@ def getYield(inFile=None, hist="", dir="", dirPre="before", bM=[0]):
   dir = dirPre + dir
   for b in bM:
     h = inFile.Get("%s/%s_%s" % (dir, hist, b))
-    # print hist
+    # print "%s/%s_%s" % (dir, hist, b)
     ent += h.GetEntries()
   
   return ent
@@ -46,20 +49,61 @@ def printPurity(before={}, matched={}):
     
     print outTxt
   print ""
+
 ###-------------------------------------------------------------------###
 
-def printEff(before={}, matched={}):
-  print ""
+def printEff(nom={}, denom={}, vetoes=[], label=""):
 
-  for process in before:
-    outTxt = formatLabel(process) + " Efficiency"
-    for i in range(len(matched[process])):
-      eff = float(matched[process][i]/beforeYld[process][i])
-      print process, matched[process][i], beforeYld[process][i]
+  effOut = {}
+  passList = []
+
+  if "Total" in label:
+    passList = ["All"]
+  else:
+    passList = ["Ele", "Mu", "HadTau", "Other"]
+
+  for process in nom:
+
+    # check if process in passList
+    if process not in passList:
+      continue
+
+    outTxt = formatLabel(process) + " Efficiency "
+    effOut[process]=[]
+
+    # if "Other" not in one of the dicts, skip
+    try:
+      tmp = nom[process]
+      if not vetoes:
+        tmp = denom[process]
+    except KeyError:
+      if "Other" in process:
+        continue
+
+    if vetoes:
+      denomVals = vetoes
+    else:
+      denomVals = denom[process]
+
+
+
+    for i in range(len(denomVals)):
+
+      try:
+        eff = float(nom[process][i]/denomVals[i])
+        # if "Other" in process:
+          # print nom[process]
+      except ZeroDivisionError:
+        # print "*** Error: Zero Division for", process
+        eff=0.
+      effOut[process].append(eff)
       outTxt += "& %.3f " % eff
 
-    print outTxt
-  print""
+    outTxt += r" \\"
+
+  makeTable(vals=effOut, label=label)
+
+  return effOut
 
 ###-------------------------------------------------------------------###
 
@@ -72,7 +116,6 @@ def formatLabel(label=""):
 
   # label = label.replace("TauEle", r"$\tau \to e \nu$")
   # label = label.replace("TauMu", r"$\tau \to \mu \nu$")
-  label = label.replace("GenTauHad", r"$\tau \to had$")
   label = label.replace("GenHadTau", r"$\tau \to had$")
   label = label.replace("GenEle", r"$\tau/W/Z \to e$")
   label = label.replace("GenMu", r"$\tau/W/Z \to \mu$")
@@ -81,8 +124,77 @@ def formatLabel(label=""):
   # label = label.replace("VEle", r"$W/Z \to e \nu$")
   # label = label.replace("VMu", r"$W/Z \to \mu \nu$")
   label = label.replace("isoTrack ", "")
+  label = label.replace("All", "Cut")
 
   return label
+
+###-------------------------------------------------------------------###
+
+def makeTable(vals={}, label=""):
+  
+  out = ""
+
+  out += printHeader()
+  out += printHT()
+
+  for key in vals:
+    out += formatLabel(key) + " "
+    for val in vals[key]:
+      out += "& %.3f " % val
+    out += r"\\"
+    out += "\n"
+
+  out += printEnd()
+
+  f = open("yieldTable_%s_%s.tex" % (switches()["sample"], label), "w")
+  f.write(out)
+  f.close()
+
+###-------------------------------------------------------------------###
+
+def printCaption():
+
+  sample = switches()["sample"].replace("_", " ")
+
+  outTxt = "\\caption{Single Isolated Track yields for %s}\n" % sample
+  #add a bit more eventually
+
+  return outTxt
+
+###-------------------------------------------------------------------###
+
+def printEnd():
+
+  outTxt = "\n\n\n"
+  outTxt += "\\end{tabular}\n"
+  outTxt += "\\end{center}\n"
+  outTxt += "\\end{table}\n"
+  outTxt += "\\end{document}"
+
+  return outTxt
+
+###-------------------------------------------------------------------###
+
+def printHT():
+
+  HTline = " HT Bins (GeV) & 200-275 & 275-325 & 325-375 & 375-475 & 475-575 & 575-675 & 675-775 & 775-875 & 875-975 & 975-$\\inf$ \\\\ \n"
+  HTline += "\hline\n"
+
+  return HTline
+
+###-------------------------------------------------------------------###
+
+def printHeader():
+  outTxt = ""
+  outTxt += "\\documentclass[a4paper,12pt]{article}\n"
+  outTxt += "\\usepackage[margin=0.3in, landscape]{geometry}\n"
+  outTxt += "\\begin{document}\n\n"
+  outTxt += "\\begin{table}[lp{5cm}l]\n"
+  outTxt += printCaption()
+  outTxt += "\\begin{center}\n"
+  outTxt += "\\begin{tabular}{ c|cccccccccc }\n"
+
+  return outTxt
 
 ###-------------------------------------------------------------------###
 
@@ -105,40 +217,72 @@ afterYld = {
       "HadTau":[],
 }
 
-matchedYld = {
+unmatchedYld = {
       "All":[],
       "Ele":[],
       "Mu":[],
       "HadTau":[],
 }
 
+matchedYld = {
+      "All":[],
+      "Ele":[],
+      "Mu":[],
+      "HadTau":[],
+      "Other":[],
+}
+
+vetoes = []
+
 HTdirs = ["200_275", "275_325", "325_375", "375_475", "475_575", "575_675", "675_775", "775_875", "875_975", "975"]
 
-iF = r.TFile.Open("../../rootfiles/isoTrackPlots/outWJets_isoTrackPlots.root")
+inFileName = "../../rootfiles/isoTrackPlots/out%s_isoTrackPlots.root" % switches()["sample"]
+iF = r.TFile.Open(inFileName)
+print "\n", inFileName
 
+### generic, before SITV yields ###
 for b in beforeYld:
   for ht in HTdirs:
     if b!="All":
-      hName = "Gen%sN" % (b if b!="HadTau" else "TauHad")
+      hName = "Gen%sN" % b
     else:
       hName = "n_Events"
 
     val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5) if b!="All" else [0])
     beforeYld[b].append(val)
 
+### generic, after SITV yields ###
 for a in afterYld:
   for ht in HTdirs:
     if a!="All":
-      hName = "Gen%sN" % (a if a!="HadTau" else "TauHad")
+      hName = "Gen%sN" % a
     else:
       hName = "n_Events"
 
     val = getYield(inFile=iF, hist=hName, dir=ht, dirPre="after", bM=range(5) if a!="All" else [0])
     afterYld[a].append(val)
 
+# get total number of vetoes
+for b, a in zip(beforeYld["All"], afterYld["All"]):
+  vetoes.append(b-a)
+
+# zero out unecessary entries
 for i in range(len(HTdirs)):
+  unmatchedYld["All"].append(0)
   matchedYld["All"].append(0)
 
+### yields for processes in events with atleast one SIT ###
+for u in unmatchedYld:
+  if u=="All": continue
+  hName = "Gen%sNoMatchN" % u
+  ctr = 0
+  for ht in HTdirs:
+    val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5) if b!="All" else [0])
+    unmatchedYld[u].append(val)
+    unmatchedYld["All"][ctr] += val
+    ctr+=1
+
+### yields for processes matched to SIT ###
 for m in matchedYld:
   if m=="All": continue
   hName = "ITGen%sN" % m
@@ -149,17 +293,28 @@ for m in matchedYld:
     matchedYld["All"][ctr] += val
     ctr+=1
 
-print beforeYld
-print ""
-print afterYld
-print ""
-print matchedYld
+
 
 if "p" in argv[1]:
   printPurity(before=beforeYld, matched=matchedYld)
 
 if "e" in argv[1]:
-  printEff(before=beforeYld, matched=matchedYld)
+  # print "\n>>> Total Efficiency"
+  # totEff = printEff(nom=afterYld, denom=beforeYld, label="Total")
+  # print totEff
+  print "\n>>> Process Efficiency"
+  procEff = printEff(nom=matchedYld, denom=beforeYld, vetoes=vetoes, label="Process")
+  print procEff["HadTau"]
+
+  # print "\n>>> Matching Efficiency"
+  # printEff(nom=matchedYld, denom=unmatchedYld)
 
 if "t" in argv[1]:
   printTotal(before=beforeYld, matched=matchedYld)
+
+HTBinEdges = []
+for ht in HTdirs:
+  val = float(ht.split("_")[0])
+  HTBinEdges.append(val)
+
+# gutils.grapher([HTBinEdges, totEff["Ele"]])
