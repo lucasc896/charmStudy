13dc0bebc83eba0b72bd3b4ee25cf3eba42d229e
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-08-21 11:44:59 +0200

    add in muon selection; new method of choosing before/after sitv; comment out nSIT function; sort out file input and output

--- anaPlots/utils/SITVYields.py
+++ anaPlots/utils/SITVYields.py
@@ -12,9 +12,12 @@ r.gROOT.SetBatch(r.kTRUE)
 def switches():
 
   switches={
-          "sample"        :["WJets", "TTbar", "DY", "ZJets", "T2cc_200_120"][2],
+          "sample"        :["WJets", "TTbar", "DY", "ZJets", "T2tt_500_1", "T2tt_825_500",
+                            "T2tt_400_1", "T2cc_200_190", "T2cc_200_120"][1],
           "HTcuts"        :["noCutInc", "standardHT","highHT","lowHT","parkedHT"][1],
-          "jetMulti"      :["le3j","ge4j","inc","after","before"][-1],
+          "jetMulti"      :["le3j","ge4j","inc"][-2],
+          "SITV"          :["before", "after"][0],
+          "sele"          :["", "onemuon", "dimuon"][1],
           "norm"          :["None", "Unitary", "xSec", "lumi"][0],
           }
 
@@ -22,16 +25,52 @@ def switches():
 
 ###-------------------------------------------------------------------###
 
-def getYield(inFile=None, hist="", dir="", dirPre="before", bM=[0]):
+def getYield(inFile=None, hist="", dir="", sitv=False, bM=[0]):
 
   ent = 0
-  dir = dirPre + dir
   for b in bM:
-    h = inFile.Get("%s/%s_%s" % (dir, hist, b))
-    # print "%s/%s_%s" % (dir, hist, b)
+    if "muon" in switches()["sele"]:
+      subStr = "sitv_" if sitv else ""
+      string = switches()["sele"]+"_"+subStr+dir+"_"+switches()["jetMulti"]
+    else:
+      subStr = "after_" if sitv else "before_"
+      string = subStr+dir+"_"+switches()["jetMulti"]
+
+    h = inFile.Get("%s/%s_%s" % (string, hist, b))
+    
+    # print "%s/%s_%s" % (string, hist, b)
     ent += h.GetEntries()
-  
+
   return ent
+
+###-------------------------------------------------------------------###
+
+# def getNSIT(inFile=None, hist="anySIT", dir="", sitv=False, bM=[0]):
+
+#   nSIT = 0
+
+#   for b in bM:
+#     if "muon" in switches()["sele"]:
+#       subStr = "sitv_" if sitv else ""
+#       string = switches()["sele"]+"_"+subStr+dir+"_"+switches()["jetMulti"]
+#     else:
+#       subStr = "after_" if sitv else "before_"
+#       string = subStr+dir+"_"+switches()["jetMulti"]
+
+#     h = inFile.Get("%s/%s_%s" % (string, hist, b))
+
+#     for i in range(h.GetNbinsX()):
+#       x = h.GetBinLowEdge(i)
+#       n = h.GetBinContent(i)
+      
+#       # cut out nSIT=0 and underflow bins
+#       if x<1.: continue
+      
+#       #nSIT += n*x
+#     nSIT += h.GetEntries()
+#   # print dir, nSIT
+#   return nSIT
+
 
 ###-------------------------------------------------------------------###
 
@@ -43,7 +82,7 @@ def printEff(nom={}, denom={}, vetoes=[], label=""):
   if "Total" in label:
     passList = ["All"]
   else:
-    passList = ["Ele", "Mu", "HadTau", "Other"]
+    passList = ["Ele", "Mu", "HadTau", "Other", "NoMatch"]
 
   for process in nom:
 
@@ -74,8 +113,7 @@ def printEff(nom={}, denom={}, vetoes=[], label=""):
 
       try:
         eff = float(nom[process][i]/denomVals[i])
-        # if "Other" in process:
-          # print nom[process]
+        # print nom[process][i], denomVals[i]
       except ZeroDivisionError:
         print "*** Error: Zero Division for", process
         eff=0.
@@ -96,15 +134,17 @@ def formatLabel(label=""):
   label = label.replace("GenEle", r"$\tau/W/Z \to e$")
   label = label.replace("GenMu", r"$\tau/W/Z \to \mu$")
   label = label.replace("IT", "IT Matched ")
-  label = label.replace("N", "")
+  # label = label.replace("N", "")
   label = label.replace("isoTrack ", "")
   label = label.replace("All", "Cut")
+
+
 
   return label
 
 ###-------------------------------------------------------------------###
 
-def makeTable(vals={}, label=""):
+def makeTable(vals={}, sample = "", label=""):
   
   out = ""
   out += printHeader(label = label)
@@ -125,15 +165,15 @@ def makeTable(vals={}, label=""):
 
   out += printEnd()
 
-  f = open("yieldTable_%s_%s.tex" % (switches()["sample"], label), "w")
+  f = open("yields/yieldTable_%s_%s.tex" % (sample, label), "w")
   f.write(out)
   f.close()
 
 ###-------------------------------------------------------------------###
 
-def printCaption(label = ""):
+def printCaption(samp = "", label = ""):
 
-  sample = switches()["sample"].replace("_", " ")
+  sample = samp.replace("_", " ")
 
   outTxt = "\\caption{Single Isolated Track %s efficiencies for %s}\n" % (label, sample)
 
@@ -184,40 +224,143 @@ def addDict(a={}, b={}):
   return a
 
 ###-------------------------------------------------------------------###
+
+###-------------------------------------------------------------------###
 ##                                                                     ##
 ###                       START OF MAIN CODE                          ###
 ##                                                                     ##
 ###-------------------------------------------------------------------###
 
-beforeYld = {
-      "All":[],
-      "Ele":[],
-      "Mu":[],
-      "HadTau":[],
-}
+def main(samp = ""):
 
-afterYld = {
-      "All":[],
-      "Ele":[],
-      "Mu":[],
-      "HadTau":[],
-}
+  beforeYld = {
+        "All":[],
+        "Ele":[],
+        "Mu":[],
+        "HadTau":[],
+        "Other":[],
+  }
 
-unmatchedYld = {
-      "Ele":[],
-      "Mu":[],
-      "HadTau":[],
-}
+  afterYld = {
+        "All":[],
+        "Ele":[],
+        "Mu":[],
+        "HadTau":[],
+  }
 
-matchedYld = {
-      "Ele":[],
-      "Mu":[],
-      "HadTau":[],
-      "Other":[],
-}
+  unmatchedYld = {
+        "Ele":[],
+        "Mu":[],
+        "HadTau":[],
+  }
 
-vetoes = []
+  matchedYld = {
+        "Ele":[],
+        "Mu":[],
+        "HadTau":[],
+        "Other":[],
+        "NoMatch":[],
+  }
 
+  vetoes = []
+
+  nSIT = []
+
+  HTdirs = ["200_275", "275_325", "325_375", "375_475", "475_575", "575_675", "675_775", "775_875", "875_975", "975"]
+
+  HTBinEdges = []
+  for ht in HTdirs:
+    val = float(ht.split("_")[0])
+    HTBinEdges.append(val)
+
+  inFileName = "../../rootfiles/isoTrackPlots/out%s_%sisoTrackPlots.root" % (samp,"muonSele_" if "muon" in switches()["sele"] else "")
+  iF = r.TFile.Open(inFileName)
+
+  # get number of SIT per ht bin
+  for ht in HTdirs:
+    nSIT.append(getYield(inFile=iF, hist="anySIT", dir=ht, bM=range(5)))
+
+  ### genLevel, before SITV yields ###
+  for b in beforeYld:
+    for ht in HTdirs:
+      if b!="All":
+        hName = "Gen%sN" % b
+      else:
+        hName = "n_Events"
+
+      val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5) if b!="All" else [0])
+      beforeYld[b].append(val)
+
+
+  ### genLevel, after SITV yields ###
+  for a in afterYld:
+    for ht in HTdirs:
+      if a!="All":
+        hName = "Gen%sN" % a
+      else:
+        hName = "n_Events"
+
+      val = getYield(inFile=iF, hist=hName, dir=ht, sitv=True, bM=range(5) if a!="All" else [0])
+      afterYld[a].append(val)
+
+
+  # get total number of vetoes from the above
+  for b, a in zip(beforeYld["All"], afterYld["All"]):
+    vetoes.append(b-a)
+
+
+  ### yields for processes in events with atleast one SIT ###
+  for u in unmatchedYld:
+    hName = "Gen%sNoMatchN" % u
+    ctr = 0
+    for ht in HTdirs:
+      val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5))
+      unmatchedYld[u].append(val)
+      ctr+=1
+
+  ### yields for processes matched to SIT ###
+  for m in matchedYld:
+    hName = "ITGen%sN" % m
+    if "NoMatch" in m:
+      hName = "ITNoMatchN"
+    ctr=0
+    for ht in HTdirs:
+      val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5))
+      matchedYld[m].append(val)
+      ctr+=1
+
+  totEff = printEff(nom=afterYld, denom=beforeYld, label="Total")
+  eventEff = printEff(nom=matchedYld, denom=beforeYld, vetoes=vetoes, label="Event")
+  procEff = printEff(nom=matchedYld, denom=beforeYld, vetoes=nSIT, label="Process")
+
+  # matchEff = printEff(nom=matchedYld, denom=unmatchedYld, label = "Matching")
+
+  # make some tables
+  makeTable(vals = addDict(totEff, procEff), sample = samp, label="Total")
+  # makeTable(vals = eventEff, sample = samp, label="Event")
+  # makeTable(vals = beforeYld, sample = samp, label="Processes")
+
+  # make a dict for graph plotting
+  graphDict = {"Total Efficiency": [HTBinEdges, totEff["All"]]}
+
+  for p in procEff:
+    # if "Other" in p: continue
+    graphDict["Process Fraction - " + formatLabel(p)] = [HTBinEdges, procEff[p]]
+
+  # plot efficiency graph
+  myGrapher = gutils.grapher(inData=graphDict, multiGraph = True)
+  myGrapher.xTitle = "H_T (GeV)"
+  myGrapher.title = "SITV Efficiencies"
+  myGrapher.outFileBase = "%s_%ssitvEffs" % (samp, "muonSele_" if "muon" in switches()["sele"] else "")
+  myGrapher.paint()
+
+  return totEff["All"]
+
+###-------------------------------------------------------------------###
+
+allDict = {}
+
+# get a better way of doing this...
 HTdirs = ["200_275", "275_325", "325_375", "375_475", "475_575", "575_675", "675_775", "775_875", "875_975", "975"]
 
 HTBinEdges = []
@@ -225,79 +368,17 @@ for ht in HTdirs:
   val = float(ht.split("_")[0])
   HTBinEdges.append(val)
 
-inFileName = "../../rootfiles/isoTrackPlots/out%s_isoTrackPlots.root" % switches()["sample"]
-iF = r.TFile.Open(inFileName)
+if "list" in str(type(switches()["sample"])):
+  for s in switches()["sample"]:
+    eff = main(s)
+    allDict[s] = [HTBinEdges, eff]
+  
+  myGrapher = gutils.grapher(inData=allDict, multiGraph = True)
+  myGrapher.xTitle = "H_T (GeV)"
+  myGrapher.title = "Total SITV Efficiencies"
+  myGrapher.outFileBase = "total_sitvEffs"
+  myGrapher.paint()
 
-
-### genLevel, before SITV yields ###
-for b in beforeYld:
-  for ht in HTdirs:
-    if b!="All":
-      hName = "Gen%sN" % b
-    else:
-      hName = "n_Events"
-
-    val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5) if b!="All" else [0])
-    beforeYld[b].append(val)
-
-
-### genLevel, after SITV yields ###
-for a in afterYld:
-  for ht in HTdirs:
-    if a!="All":
-      hName = "Gen%sN" % a
-    else:
-      hName = "n_Events"
-
-    val = getYield(inFile=iF, hist=hName, dir=ht, dirPre="after", bM=range(5) if a!="All" else [0])
-    afterYld[a].append(val)
-
-
-# get total number of vetoes from the above
-for b, a in zip(beforeYld["All"], afterYld["All"]):
-  vetoes.append(b-a)
-
-
-### yields for processes in events with atleast one SIT ###
-for u in unmatchedYld:
-  hName = "Gen%sNoMatchN" % u
-  ctr = 0
-  for ht in HTdirs:
-    val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5))
-    unmatchedYld[u].append(val)
-    ctr+=1
-
-### yields for processes matched to SIT ###
-for m in matchedYld:
-  hName = "ITGen%sN" % m
-  ctr=0
-  for ht in HTdirs:
-    val = getYield(inFile=iF, hist=hName, dir=ht, bM=range(5))
-    matchedYld[m].append(val)
-    ctr+=1
-
-
-totEff = printEff(nom=afterYld, denom=beforeYld, label="Total")
-eventEff = printEff(nom=matchedYld, denom=beforeYld, vetoes=vetoes, label="Event")
-procEff = printEff(nom=matchedYld, denom=beforeYld, label="Process")
-
-# matchEff = printEff(nom=matchedYld, denom=unmatchedYld, label = "Matching")
-
-# make some tables
-makeTable(vals = addDict(totEff, procEff), label="Total")
-makeTable(vals = eventEff, label="Event")
-
-# make a dict for graph plotting
-graphDict = {"Total Efficiency": [HTBinEdges, totEff["All"]]}
-
-for p in procEff:
-  if "Other" in p: continue
-  graphDict["Process Efficiency - " + formatLabel(p)] = [HTBinEdges, procEff[p]]
-
-# plot efficiency graph
-myGrapher = gutils.grapher(inData=graphDict, multiGraph = True)
-myGrapher.xTitle = "H_T (GeV)"
-myGrapher.title = "SITV Efficiencies"
-myGrapher.outFileBase = "%s_sitvEffs" % switches()["sample"]
-myGrapher.paint()
+else:
+  main(switches()["sample"])
 
